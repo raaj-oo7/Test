f07f10801f919a713256fb837812eb6b1ed5928c
raaj-oo7 <raj.makadia22@gmail.com>
2023-10-11 18:37:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const redButton = document.getElementById("redButton");
+const greenButton = document.getElementById("greenButton");
+const blueButton = document.getElementById("blueButton");
+
+const redCount = document.getElementById("redCount");
+const greenCount = document.getElementById("greenCount");
+const blueCount = document.getElementById("blueCount");
+const totalCount = document.getElementById("totalCount");
+
+let redClickCount = 0;
+let greenClickCount = 0;
+let blueClickCount = 0;
+let totalClickCount = 0;
+
+redButton.addEventListener("click", () => {
+    redClickCount++;
+    totalClickCount++;
+
+    totalCount.textContent = totalClickCount;
+    redCount.textContent = redClickCount;
+
+    if (redClickCount % 2 === 1) {
+        redButton.classList.remove("lightRed");
+        redButton.classList.add("darkRed");
+    } else {
+        redButton.classList.remove("darkRed");
+        redButton.classList.add("lightRed");
+    }
+});
+
+greenButton.addEventListener("click", () => {
+    greenClickCount++;
+    totalClickCount++;
+
+    totalCount.textContent = totalClickCount;
+    greenCount.textContent = greenClickCount;
+
+    if (greenClickCount % 3 === 1) {
+        redButton.classList.remove("darkRed");
+        redButton.classList.add("darkRed");
+    } else if (greenClickCount % 3 === 2) {
+        redButton.classList.remove("darkRed");
+        greenButton.classList.add("darkGreen");
+    } else {
+        greenButton.classList.remove("darkGreen");
+    }
+});
+
+blueButton.addEventListener("click", () => {
+    blueClickCount++;
+    totalClickCount++;
+
+    totalCount.textContent = totalClickCount;
+    blueCount.textContent = blueClickCount;
+
+    if (blueClickCount % 4 === 1) {
+        redButton.classList.add("darkRed");
+    } else if (blueClickCount % 4 === 2) {
+        redButton.classList.remove("darkRed");
+        greenButton.classList.add("darkGreen");
+    } else if (blueClickCount % 4 === 3) {
+        greenButton.classList.remove("darkGreen");
+        blueButton.classList.add("darkBlue");
+    } else {
+        blueButton.classList.remove("darkBlue");
+        blueButton.classList.add("lightBlue");
+    }
+});
